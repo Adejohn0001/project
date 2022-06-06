f32cba8ac37368040438b757326881fd4ac35f6c
Ojo Oluwapelumi <oluwapelumiojo05@gmail.com>
2022-06-06 11:37:24 +0100

    My django project file with my zuri username as the name of the project.

---
+++ Oluwapelumiojo/newapp/views.py
@@ -0,0 +1,10 @@
+from cgitb import text
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+
+def hello(request):
+    text=""
+    "<h1>Welcome to my app!</h1>"
+    return HttpResponse(text)
